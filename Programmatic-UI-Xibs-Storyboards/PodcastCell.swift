04f771f3ff1bcf1b9b82ff063fb0bad4b493f12a
David <lin.david985@gmail.com>
2020-01-29 17:23:46 -0500

    Initial Commit

---
+++ Programmatic-UI-Xibs-Storyboards/PodcastCell.swift
@@ -0,0 +1,14 @@
+//
+//  PodcastCell.swift
+//  Programmatic-UI-Xibs-Storyboards
+//
+//  Created by David Lin on 1/29/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class PodcastCell: UICollectionViewCell {
+    @IBOutlet weak var podcastImageView: UIImageView!
+    
+}
---
+++ Programmatic-UI-Xibs-Storyboards/PodcastView.swift
@@ -0,0 +1,51 @@
+//
+//  PodcastView.swift
+//  Programmatic-UI-Xibs-Storyboards
+//
+//  Created by Alex Paul on 1/29/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class PodcastView: UIView {
+    
+   // collection view
+    // use lazy so it only gets created/ instantiated when it gets called
+    public lazy var collectionView: UICollectionView = {
+       let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: 400, height: 400)
+        let cv = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        cv.backgroundColor = .lightGray
+        return cv
+    }()
+    
+    
+
+  override init(frame: CGRect) {
+    super.init(frame: UIScreen.main.bounds)
+    commonInit()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    commonInit()
+  }
+  
+  private func commonInit() {
+    setupCollectionViewConstraints()
+  }
+    
+    private func setupCollectionViewConstraints() {
+        addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        
+        ])
+    }
+}
